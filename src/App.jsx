c514a7f42a3257589c4609d9fee0aaa989a2260f
szuiderweg <svmzuiderweg@hotmail.com>
2024-01-25 22:05:24 +0100

    project gekloond, uitwerkingen branch gemaakt, HTML basisstructuur in app.jsx toegevoegd

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,45 @@ import './App.css';
 
 function App() {
   return (
-    <h1>Begin hier met met maken van de applicatie!</h1>
+  <>
+      <header>
+        <h1>Tech it easy dashboard</h1>
+      </header>
+      <main>
+        <section>
+            <h2>Verkoopoverzicht</h2>
+            <div>
+                <article>
+                    <h3>Aantal verkochte producten</h3>
+                    <p>##</p>
+                </article>
+
+                <article>
+                    <h3>Aantal ingekochte producten</h3>
+                    <p>##</p>
+                </article>
+
+                <article>
+                    <h3>Aantal te verkopen producten</h3>
+                    <p>##</p>
+                </article>
+            </div>
+        </section>
+
+          <section>
+              <h2>Best verkochte tv</h2>
+              <article>
+                  <div><img src="/" alt="tv"/></div>
+                  <div>
+                      <p>productnaam</p>
+                      <p>prijs</p>
+                      <p>afmetingen</p>
+                      <p>feature bolletjes</p>
+                  </div>
+              </article>
+          </section>
+      </main>
+  </>
   )
 }
 
