c0c9fd3069b3080b791993c45b56b651932303c5
szuiderweg <svmzuiderweg@hotmail.com>
2024-03-02 22:41:09 +0100

    deel 2, opdracht 1 helemaal werkend als los JS bestand

---
+++ src/helpers/tvExperiment.js
@@ -0,0 +1,20 @@
+import {inventory} from "../constants/inventory.js";
+
+
+//experiment-functie  om opdracht 1e werkend te krijgen
+//stapjes:
+// for loop een array met true en false uit laten spugen
+//
+
+//test de binnenste forloop
+//in: 1 tv uit inventory
+//uit: per tv object true of false of hij groot genoeg is
+let tv = inventory[1];
+let groot = false;
+for(let k = 0; k < 10;k++)
+{
+    console.log(k);
+}
+
+//1f : Gebruik array-methoden om alle informatie te verzamelen van de tv's die over ambilight beschikken. Log de uitkomst in de console
+
